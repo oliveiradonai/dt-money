20ae9c8fc5454faf6bc3b60e77be6aac1a908a58
oliveiradonai <adonai.fig@gmail.com>
2022-09-29 22:08:03 -0300

    <feat>: Nova URL Produção

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'https://adnfig-dt-money.vercel.app/api',
+    baseURL: 'https://dt-money.adnfig.me/api',
     // baseURL: 'http://localhost:3000/api',
 })
